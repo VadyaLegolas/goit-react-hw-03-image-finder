922c3008893811b7e63b23dcb055d6326b1c5acf
Vlad <vadya-85@mail.ru>
2024-01-07 12:55:10 +0100

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,22 @@
-import { Component } from 'react';
 import { Hourglass } from 'react-loader-spinner';
 import { LoaderDiv } from './Loader.styled';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <LoaderDiv>
-        <Hourglass
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="hourglass-loading"
-          wrapperStyle={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          wrapperClass=""
-          colors={['#306cce', '#72a1ed']}
-        />
-      </LoaderDiv>
-    );
-  }
-}
+export const Loader = () => {
+  return (
+    <LoaderDiv>
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        wrapperClass=""
+        colors={['#306cce', '#72a1ed']}
+      />
+    </LoaderDiv>
+  );
+};
